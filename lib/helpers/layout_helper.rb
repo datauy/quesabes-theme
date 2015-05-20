1c76cf737cdfede5d0d83460d453681084bfd86a
Fernando Briano <fernando@picandocodigo.net>
2015-05-20 19:01:29 -0300

    General fixes, status classes

--- lib/helpers/layout_helper.rb
+++ lib/helpers/layout_helper.rb
@@ -11,4 +11,20 @@ module LayoutHelper
   def logo_class
     is_root? ? "logo-blanco" : "logo-color"
   end
+
+  def show_status(status)
+    css = if ["internal_review", "waiting_response", "waiting_clarification",
+              "requires_admin", "not_held"].include? status
+            "delate"
+          elsif[].include? status
+            "waiting"
+          elsif ["error_message", "rejected", "unsatisfactory_response",
+                 "gone_postal", "user_withdrawn"].include? status
+            "wrong"
+          elsif ["successful", "partially_successful"].include? status
+            "success"
+          end
+    css
+    "<h4 class=\"status #{css}\">#{status}</h4>".html_safe
+  end
 end
