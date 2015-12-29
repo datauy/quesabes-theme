d38d7663223fc26ff876449205cf33e50db18f25
Alf Cora <alfius@protonmail.com>
2015-12-29 10:33:10 -0300

    Add color codes for all the possible states and reuse them across the site

--- lib/helpers/layout_helper.rb
+++ lib/helpers/layout_helper.rb
@@ -12,21 +12,6 @@ module LayoutHelper
     is_root? ? "logo-blanco" : "logo-color"
   end
 
-  def show_status(status)
-    css = if ["internal_review", "requires_admin", "not_held"].include? status
-            "delate"
-          elsif["waiting_response", "waiting_clarification"].include? status
-            "waiting"
-          elsif ["error_message", "rejected", "unsatisfactory_response",
-                 "gone_postal", "user_withdrawn"].include? status
-            "wrong"
-          elsif ["successful", "partially_successful"].include? status
-            "success"
-          end
-    css
-    "<h4 class=\"status #{css}\"> #{ _(status)}</h4>".html_safe
-  end
-
   def public_body_follower_count(public_body)
     TrackThing.count(
       :all,
