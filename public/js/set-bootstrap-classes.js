8ddc176fdfea57617ec95954b44dc855be60ee38
Alf Cora <alfius@protonmail.com>
2015-12-30 12:35:08 -0300

    More polishing to the request flow

---
+++ public/js/set-bootstrap-classes.js
@@ -0,0 +1,8 @@
+$(function (){
+  $('#outgoing_message_body').addClass('form-control');
+  $('#upload_response_form textarea').addClass('form-control');
+  $('#upload_response_form #file_1').addClass('form-control');
+  $('#upload_response_form input[type=submit]').removeClass('btn-default').addClass('btn btn-success');
+  $('#comment_form #comment_body').addClass('form-control');
+  $('#comment_form input[type=submit]').removeClass('btn-default').addClass('btn btn-success');
+});
