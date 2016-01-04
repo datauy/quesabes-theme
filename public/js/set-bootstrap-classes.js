b30fa68eb6d59f4248bad2db80e7534908b78a18
Alf Cora <alfius@protonmail.com>
2016-01-04 15:20:18 -0300

    Style admin login form, file inputs and fix link to all requests

--- public/js/set-bootstrap-classes.js
+++ public/js/set-bootstrap-classes.js
@@ -5,4 +5,7 @@ $(function (){
   $('#upload_response_form input[type=submit]').removeClass('btn-default').addClass('btn btn-success');
   $('#comment_form #comment_body').addClass('form-control');
   $('#comment_form input[type=submit]').removeClass('btn-default').addClass('btn btn-success');
+  $('#incoming_message_message').addClass('form-control');
+  $('#sign_alone #user_signin_email').addClass('form-control');
+  $('#sign_alone #user_signin_password').addClass('form-control');
 });
