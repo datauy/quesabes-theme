c983ec57de7028dadcfd9f661251038417cf89ae
Alf Cora <alfius@protonmail.com>
2016-01-11 13:55:02 -0300

    Set the default letter

--- lib/helpers/layout_helper.rb
+++ lib/helpers/layout_helper.rb
@@ -18,4 +18,18 @@ module LayoutHelper
       conditions: ["public_body_id = ?", public_body.id]
     )
   end
+
+  def default_letter
+    %{De mi mayor consideración,
+Me dirijo a esta Oficina a los efectos de realizar una pedido de
+acceso a la información pública, conforme a la ley 18381. Solicito me entregue:
+
+[PEDIDO]
+
+A estos efectos, solicito que se comunique conmigo mediante la
+dirección de correo establecida en este pedido, fijando mi
+domicilio en [DIRECCION]. A su vez solicito que
+la entrega de la información sea preferentemente en formato
+electrónico.}
+  end
 end
