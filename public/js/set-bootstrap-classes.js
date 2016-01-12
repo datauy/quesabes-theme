b36266b6de9f79dc9ba746cddb0855068efe5545
Alf Cora <alfius@protonmail.com>
2016-01-12 17:51:07 -0300

    Style the form to change a public body email

--- public/js/set-bootstrap-classes.js
+++ public/js/set-bootstrap-classes.js
@@ -8,4 +8,11 @@ $(function (){
   $('#incoming_message_message').addClass('form-control');
   $('#sign_alone #user_signin_email').addClass('form-control');
   $('#sign_alone #user_signin_password').addClass('form-control');
+  $('#new_public_body_change_request #public_body_change_request_user_name').addClass('form-control');
+  $('#new_public_body_change_request #public_body_change_request_user_email').addClass('form-control');
+  $('#new_public_body_change_request #public_body_change_request_public_body_email').addClass('form-control');
+  $('#new_public_body_change_request #public_body_change_request_source_url').addClass('form-control');
+  $('#new_public_body_change_request #public_body_change_request_notes').addClass('form-control');
+  $('#new_public_body_change_request .form_item_note').addClass('help-block');
+  $('#new_public_body_change_request input[type=submit]').removeClass('btn-default').addClass('btn btn-success');
 });
